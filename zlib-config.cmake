b247492c37ca9204907d09d0a5593374679e8488
Scott M Anderson <smanders@sdl.usu.edu>
2014-12-03 16:05:26 -0700

    ZLIB_INCLUDE_DIRS to match FindZLIB.cmake

--- zlib-config.cmake
+++ zlib-config.cmake
@@ -1,5 +1,5 @@
 # ZLIB_FOUND - zlib was found
-# ZLIB_INCLUDE_DIR - the zlib include directory
+# ZLIB_INCLUDE_DIRS - the zlib include directory
 # ZLIB_LIBRARIES - the zlib libraries
 set(prj zlib)
 # this file (-config) installed to share/cmake by default (see top-level CMakeLists.txt)
@@ -8,12 +8,12 @@ get_filename_component(XP_ROOTDIR "${SELF_DIR}/../../" ABSOLUTE)
 # targets file (-targets) installed to lib${NUMBITS}/cmake
 include(${XP_ROOTDIR}/lib${NUMBITS}/cmake/${prj}-targets.cmake)
 string(TOUPPER ${prj} PRJ)
-find_path(${PRJ}_INCLUDE_DIR zlib.h PATHS ${XP_ROOTDIR}/include/${prj} NO_DEFAULT_PATH)
+find_path(${PRJ}_INCLUDE_DIRS zlib.h PATHS ${XP_ROOTDIR}/include/${prj} NO_DEFAULT_PATH)
 set(${PRJ}_LIBRARIES zlibstatic)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(${prj}
   REQUIRED_VARS
-    ${PRJ}_INCLUDE_DIR
+    ${PRJ}_INCLUDE_DIRS
     ${PRJ}_LIBRARIES
   )
-mark_as_advanced(${PRJ}_INCLUDE_DIR ${PRJ}_LIBRARIES)
+mark_as_advanced(${PRJ}_INCLUDE_DIRS ${PRJ}_LIBRARIES)
